7aa094333227dc9130cf86586888c8a5955afae2
Apparition2018 <88850180@163.com>
2023-01-31 16:48:31 +0800

    2023-01-31 16:48:00

--- activiti5/src/test/java/com/ljh/ActivitiTest.java
+++ activiti5/src/test/java/com/ljh/ActivitiTest.java
@@ -2,8 +2,12 @@ package com.ljh;
 
 import lombok.extern.slf4j.Slf4j;
 import org.activiti.engine.*;
+import org.activiti.engine.history.HistoricProcessInstance;
+import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
+import org.activiti.engine.test.ActivitiRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -36,7 +40,7 @@ public class ActivitiTest {
                 .setJdbcUrl("jdbc:h2:mem:activiti;DB_CLOSE_DELAY=1000")
                 .setJobExecutorActivate(Boolean.TRUE)
                 .buildProcessEngine();
-        
+
         ProcessEngines.init();
     }
 
@@ -63,11 +67,11 @@ public class ActivitiTest {
         variables.put("vacationMotivation", "I'm really tired!");
         RuntimeService runtimeService = processEngine.getRuntimeService();
         ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("vacationRequest", variables);
-        log.warn("Number of process instances: {}", runtimeService.createProcessInstanceQuery().count());
+        log.warn("Number of process instances: " + runtimeService.createProcessInstanceQuery().count());
         // 暂停/激活 流程实例
         runtimeService.suspendProcessInstanceById(processInstance.getId());
         runtimeService.activateProcessInstanceById(processInstance.getId());
-        
+
         // 完成任务
         TaskService taskService = processEngine.getTaskService();
         List<Task> tasks = taskService.createTaskQuery().taskCandidateGroup("management").list();
@@ -82,6 +86,12 @@ public class ActivitiTest {
     }
 
     /**
+     * <a href="https://www.activiti.org/5.x/userguide/#apiUnitTesting">单元测试</a>
+     */
+    @Rule
+    public ActivitiRule activitiRule = new ActivitiRule();
+
+    /**
      * <a href="https://www.activiti.org/5.x/userguide/#_transactions">Spring 事务集成</a>
      */
     @Test
@@ -89,5 +99,44 @@ public class ActivitiTest {
         ClassPathXmlApplicationContext applicationContext =
                 new ClassPathXmlApplicationContext("SpringTransactionIntegrationTest-context.xml");
         RepositoryService repositoryService = (RepositoryService) applicationContext.getBean("repositoryService");
+    }
+
+    /**
+     * <a href="https://www.activiti.org/5.x/userguide/#_getting_started_10_minute_tutorial">Demo</a>
+     */
+    @Test
+    public void testDemo() {
+        ProcessEngine processEngine = ProcessEngineConfiguration.createStandaloneInMemProcessEngineConfiguration().buildProcessEngine();
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        TaskService taskService = activitiRule.getTaskService();
+        HistoryService historyService = activitiRule.getHistoryService();
+
+        // 部署流程定义
+        Deployment deployment = repositoryService.createDeployment()
+                .addClasspathResource("processes/FinancialReportProcess.bpmn20.xml")
+                .deploy();
+
+        // 启动流程实例，并返回实例 ID
+        String procId = runtimeService.startProcessInstanceByKey("financialReport").getId();
+
+        // 查询 accountancy 组的任务
+        List<Task> tasks = taskService.createTaskQuery().taskCandidateGroup("accountancy").list();
+        // 分配任务给 fozzie
+        tasks.forEach(t -> taskService.claim(t.getId(), "fozzie"));
+        // 查询分配给 fozzie 的任务
+        tasks = taskService.createTaskQuery().taskAssignee("fozzie").list();
+        // 完成任务
+        tasks.forEach(t -> taskService.complete(t.getId()));
+        // 查询 management 组的任务
+        tasks = taskService.createTaskQuery().taskCandidateGroup("management").list();
+        // 分配任务给 kermit
+        tasks.forEach(t -> taskService.claim(t.getId(), "kermit"));
+        // 完成任务
+        tasks.forEach(t -> taskService.complete(t.getId()));
+
+        // 根据流程例 ID 查询历史记录
+        HistoricProcessInstance historicProcessInstance = historyService.createHistoricProcessInstanceQuery().processInstanceId(procId).singleResult();
+        log.warn("Process instance end time: " + historicProcessInstance.getEndTime());
     }
 }
